79042028ab2961cd3a425004d9ea3e0cd678cbf3
Dedu <dominguezudaniele@gmail.com>
2025-09-22 11:41:32 -0600

    chore: unfollow env, connection and vite config files

--- server/database/connection.ts
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from "sequelize";
-
-const db = new Sequelize("usc_database", "root", "#jc@kfYkp7Hc", {
-  host: "localhost",
-  dialect: "mysql",
-  logging: false,
-});
-
-export default db;
